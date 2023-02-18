6b131d3bcd93b5d23e010486ff0a10375b9e11e0
Charlotte <cxl229@nyu.edu>
2023-02-18 15:05:15 -0500

    starting collision detection code

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,41 +1,47 @@
-// citation: https://pixijs.io/guides/basics/getting-started.html
-// import * as PIXI from 'pixi.js'
-
 // ----- Set up the pixi app and canvas -----
-const gameWidth = 700, gameHeight = 500;
+const gameWidth = 1000, gameHeight = 600;
 let app = new PIXI.Application({width: gameWidth, height: gameHeight});
 document.querySelector("#canvas").appendChild(app.view);
 app.renderer.background.color = 0x123456;
 
-// Citation: https://medium.com/swlh/a-game-any-web-dev-can-build-in-10-mins-using-pixijs-47f8bcd85700
-// Player class
+// ----- Classes for game objects -----
+// Player object
 class Player {
     constructor(sprite, speed) {
         this.sprite = sprite;
         this.speed = speed;
         this.velocity = {x:0, y:0};
-        app.stage.addChild(this.sprite);
     }
 
     update() {
         let x = this.sprite.x + this.velocity.x;
         let y = this.sprite.y + this.velocity.y;
 
-        this.sprite.x = Math.min(Math.max(x, 0), gameWidth-32); // TODO hard coded size of sprite for now
-        this.sprite.y = Math.min(Math.max(y, 0), gameHeight-32);
+        this.sprite.x = Math.min(Math.max(x, 0), gameWidth-64);
+        this.sprite.y = Math.min(Math.max(y, 0), gameHeight-64);
+    }
+}
+
+// Structure
+class Structure {
+    constructor(sprite) {
+        this.sprite = sprite;
     }
 }
 
 // ----- Set up the player -----
-function setUpPlayerControls() {
-    window.addEventListener("keydown", onKeydown);
-    window.addEventListener("keyup", onKeyUp);
-}
-
 let pressed = {};
 let player;
-player = new Player(PIXI.Sprite.from('test-sprite.png'), 10);
+player = new Player(PIXI.Sprite.from('test-sprite.png'), 8);
+app.stage.addChild(player.sprite);
 setUpPlayerControls();
+
+// Player control
+// Citation: https://medium.com/swlh/a-game-any-web-dev-can-build-in-10-mins-using-pixijs-47f8bcd85700
+function setUpPlayerControls() {
+    window.addEventListener("keydown", onKeydown);
+    window.addEventListener("keyup", onKeyup);
+}
 
 function onKeydown(event) {
     switch (event.key) {
@@ -65,7 +71,7 @@ function onKeydown(event) {
     }
 }
 
-function onKeyUp(event) {
+function onKeyup(event) {
     switch (event.key) {
         case "ArrowLeft": 
         case "a":
@@ -93,8 +99,26 @@ function onKeyUp(event) {
     }
 }
 
-// ----- Game loop -----
+// Set up a building
+let building = new Structure(PIXI.Sprite.from('blue_rectangle.jpg'));
+app.stage.addChild(building.sprite);
+building.sprite.x = 100;
+building.sprite.y = 200;
+
+// ----- Collision detection -----
+// Citation: https://codepen.io/hsiangfeng/embed/RwNLBWP?height=265&theme-id=default&default-tab=js,result
+function isColliding(a, b) {
+  const ab = a.getBounds();
+  const bb = b.getBounds();
+  return ab.x + ab.width > bb.x && ab.x < bb.x + bb.width && ab.y + ab.height > bb.y && ab.y < bb.y + bb.height;
+}
+
+// ----- Game loop to actually run the game -----
 setInterval(gameLoop, 1000/60);
 function gameLoop() {
     player.update();
+
+    if (isColliding(player.sprite, building.sprite)) {
+        player.speed = -player.speed;
+    }
 }
