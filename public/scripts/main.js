2174527e61becf19f9d125db66ef9622d54f579b
Charlotte <cxl229@nyu.edu>
2023-02-18 17:54:32 -0500

    auto scrolling of window, dialogue box when interacting with environmental objects

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,5 @@
 // ----- Set up the pixi app and canvas -----
-const gameWidth = 1000, gameHeight = 600;
+const gameWidth = 2400, gameHeight = 1600;
 let app = new PIXI.Application({width: gameWidth, height: gameHeight});
 document.querySelector("#canvas").appendChild(app.view);
 app.renderer.background.color = 0x123456;
@@ -20,6 +20,14 @@ class Player {
         this.sprite.x = Math.min(Math.max(x, 0), gameWidth-64);
         this.sprite.y = Math.min(Math.max(y, 0), gameHeight-64);
     }
+
+    freeze() {
+        this.speed = 0;
+    }
+
+    unfreeze() {
+        this.speed = playerDefaultSpeed;
+    }
 }
 
 // Structure
@@ -30,9 +38,10 @@ class Structure {
 }
 
 // ----- Set up the player -----
+const playerDefaultSpeed = 8;
 let pressed = {};
 let player;
-player = new Player(PIXI.Sprite.from('test-sprite.png'), 8);
+player = new Player(PIXI.Sprite.from('test-sprite.png'), playerDefaultSpeed);
 app.stage.addChild(player.sprite);
 setUpPlayerControls();
 
@@ -48,25 +57,33 @@ function onKeydown(event) {
         case "ArrowLeft":
         case "a":
             player.velocity.x = -player.speed; 
-            pressed['left'] = true;
+            pressed.left = true;
             break;
 
         case "ArrowRight":
         case "d":
             player.velocity.x = player.speed; 
-            pressed['right'] = true;
+            pressed.right = true;
             break;
 
         case "ArrowUp":
         case "w":
             player.velocity.y = -player.speed; 
-            pressed['up'] = true;
+            pressed.up = true;
             break;
 
         case "ArrowDown": 
         case "s":
             player.velocity.y = player.speed; 
-            pressed['down'] = true;
+            pressed.down = true;
+            break;
+
+        case "e":
+            pressed.e = true;
+            break;
+
+        case "x":
+            pressed.x = true;
             break;
     }
 }
@@ -75,26 +92,34 @@ function onKeyup(event) {
     switch (event.key) {
         case "ArrowLeft": 
         case "a":
-            player.velocity.x = pressed['right'] ? player.speed:0; 
-            pressed['left'] = false;
+            player.velocity.x = pressed.right ? player.speed:0; 
+            pressed.left = false;
             break;
 
         case "ArrowRight": 
         case "d":
-            player.velocity.x = pressed['left'] ? -player.speed:0; 
-            pressed['right'] = false;
+            player.velocity.x = pressed.lef ? -player.speed:0; 
+            pressed.right = false;
             break;
 
         case "ArrowUp": 
         case "w":
-            player.velocity.y = pressed['down'] ? player.speed:0; 
-            pressed['up'] = false;
+            player.velocity.y = pressed.down ? player.speed:0; 
+            pressed.up = false;
             break;
 
         case "ArrowDown": 
         case "s":
-            player.velocity.y = pressed['up'] ? -player.speed:0; 
-            pressed['down'] = false;
+            player.velocity.y = pressed.up ? -player.speed:0; 
+            pressed.down = false;
+            break;
+
+        case "e":
+            pressed.e = false;
+            break;
+
+        case "x":
+            pressed.x = false;
             break;
     }
 }
@@ -105,12 +130,16 @@ app.stage.addChild(building.sprite);
 building.sprite.x = 100;
 building.sprite.y = 200;
 
+// Set up the dialogue box
+let dialogueBox = PIXI.Sprite.from('tan-rectangle.png');
+app.stage.addChild(dialogueBox);
+dialogueBox.x = window.innerWidth/2 - 1000/2; // TODO length of dialogue box
+dialogueBox.y = window.innerHeight - 171 - 20; // TODO height of the dialogue box
+dialogueBox.visible = false;
+
 // ----- Collision detection -----
-// Citation: https://codepen.io/hsiangfeng/embed/RwNLBWP?height=265&theme-id=default&default-tab=js,result
 function isColliding(a, b) {
-  const ab = a.getBounds();
-  const bb = b.getBounds();
-  return ab.x + ab.width > bb.x && ab.x < bb.x + bb.width && ab.y + ab.height > bb.y && ab.y < bb.y + bb.height;
+    return a.getBounds().intersects(b.getBounds());
 }
 
 // ----- Game loop to actually run the game -----
@@ -118,7 +147,34 @@ setInterval(gameLoop, 1000/60);
 function gameLoop() {
     player.update();
 
-    if (isColliding(player.sprite, building.sprite)) {
-        player.speed = -player.speed;
+    if (isColliding(player.sprite, building.sprite) && pressed.e) {
+        dialogueBox.visible = true;
+        player.freeze();
+    }
+
+    if (dialogueBox.visible && pressed.x) {
+        dialogueBox.visible = false;
+        player.unfreeze();
+    }
+
+    // ----- Auto scroll window -----
+    // player has exited window right
+    if (pressed.right && player.sprite.x > window.innerWidth/2) {
+        window.scrollBy(playerDefaultSpeed, 0);
+    }
+    
+    // player has exited window down
+    if (pressed.down && player.sprite.y > window.innerHeight/2) {
+        window.scrollBy(0, playerDefaultSpeed);
+    }
+
+    // player has exited window left
+    if (pressed.left && player.sprite.x < window.innerWidth) {
+        window.scrollBy(-playerDefaultSpeed, 0);
+    }
+
+    // player has exited window up
+    if (pressed.up && player.sprite.y < window.innerHeight * 1.5) {
+        window.scrollBy(0, -playerDefaultSpeed);
     }
 }
